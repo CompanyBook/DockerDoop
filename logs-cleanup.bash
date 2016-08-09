1af8925211d48dedbb670f551b7f75b387d54434
Oleg Blinnikov <oblinnikov@gmail.com>
2016-08-09 14:54:03 +0000

    cleanup

---
+++ logs-cleanup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONTAINERID='2ef9290c69d2'
+docker exec $CONTAINERID rm /var/log/hbase/gc.log-201607190854
+docker exec $CONTAINERID rm /var/log/hbase/SecurityAuth.audit
+docker exec $CONTAINERID rm /var/log/hbase/hbase-hbase-master-namenode.log
+docker exec $CONTAINERID rm /var/log/hbase/hbase-hbase-regionserver-namenode.log
+docker exec $CONTAINERID du -h /var/log/hbase
