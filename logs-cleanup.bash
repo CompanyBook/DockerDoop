789473933c1471a96f808d80828dcc7aa4ce54bd
Oleg Blinnikov <oblinnikov@gmail.com>
2016-09-08 11:14:57 +0000

    print how much disk occupied befoure cleanup

--- logs-cleanup.bash
+++ logs-cleanup.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CONTAINERID='2ef9290c69d2'
+docker exec $CONTAINERID du -h /var/log/hbase
 docker exec $CONTAINERID rm /var/log/hbase/gc.log-201607190854
 docker exec $CONTAINERID rm /var/log/hbase/SecurityAuth.audit
 docker exec $CONTAINERID rm /var/log/hbase/hbase-hbase-master-namenode.log
