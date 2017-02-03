448f29d89655c554ab71e6e084d70d5b18fe498c
Oleg Blinnikov <osblinnikov@gmail.com>
2017-02-03 09:39:49 +0100

    use ip address to start services

--- scripts/startCluster.sh
+++ scripts/startCluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+namenode=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -1`
+
 if [[ -z $1 ]]; then
   echo "Usage: $0 <configuration.ini> "
   exit -1
@@ -11,12 +13,12 @@ clusterName=$(awk -F "=" '/clusterName/ {print $2}' $iniFile)
 cont=$(docker ps -a | grep ".*.$clusterName$CONTAINER_POSTFIX" | cut -f1 -d" ")
 docker start $cont
 docker exec $cont sudo service ambari start
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HDFS via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/HDFS
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start YARN via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/YARN
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start MAPREDUCE2 via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/MAPREDUCE2
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start ZOOKEEPER via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/ZOOKEEPER
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HBASE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/HBASE
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start KAFKA via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/KAFKA
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HIVE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/HIVE
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start SPARK via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/SPARK
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start STORM via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://namenode:58080/api/v1/clusters/$clusterName/services/STORM
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HDFS via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/HDFS
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start YARN via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/YARN
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start MAPREDUCE2 via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/MAPREDUCE2
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start ZOOKEEPER via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/ZOOKEEPER
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HBASE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/HBASE
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start KAFKA via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/KAFKA
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HIVE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/HIVE
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start SPARK via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/SPARK
+curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start STORM via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$namenode:58080/api/v1/clusters/$clusterName/services/STORM
