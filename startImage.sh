6d43f1a46dce8882498191abfb2659002e64f126
Oleg Blinnikov <osblinnikov@gmail.com>
2017-02-03 09:34:00 +0100

    auto identify IP

--- startImage.sh
+++ startImage.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+CURRENT_IP=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -1`
+cat singlenode-sample.ini.origin | sed "s/localhost/$CURRENT_IP/g" > singlenode-sample.ini
+
 POSTFIX=${1:-2}
 IMAGE=${2:-companybookbuild-002.servers.prgn.misp.co.uk:5000/singlenode-hdp}
 
